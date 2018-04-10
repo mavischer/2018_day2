387b781cbecc06099b3242b31046826a913ce470
Marc Aurel Vischer <vischer@campus.tu-berlin.de>
2018-04-10 10:53:40 +0200

    fixed maximum on left border

--- maxima.py
+++ maxima.py
@@ -12,13 +12,17 @@ def find_maxima(x):
     Output:
         idx -- list of indices of the local maxima in x
     """
+    # `i` is a local maximum if the signal decreases before and after it
 
     idx = []
     for i in range(len(x)):
         if i == len(x) - 1:
             if x[i-1] < x[i]:
                 idx.append(i)
-        # `i` is a local maximum if the signal decreases before and after it
+        elif i==0:
+            if x[i+1] < x[i]:
+                idx.append(i)
         elif x[i-1] < x[i] and x[i+1] < x[i]:
             idx.append(i)
+
     return idx
